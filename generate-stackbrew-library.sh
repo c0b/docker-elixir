e9f7f41f85a1e3e6f0dfe66cff15506708592a72
18年梦醒 <3949379+getong@users.noreply.github.com>
2019-05-28 13:45:55 +0800

    export erlang/otp 22 and delete erlang/otp 21.2

--- generate-stackbrew-library.sh
+++ generate-stackbrew-library.sh
@@ -71,7 +71,7 @@ for version in "${versions[@]}"; do
 	done
 	versionAliases+=( $version ${aliases[$version]:-} )
 
-	for variant in '' slim alpine otp-{21,21.2}{,-alpine}; do
+	for variant in '' slim alpine otp-{21,22}{,-alpine}; do
 		dir="$version${variant:+/$variant}"
 		[ -f "$dir/Dockerfile" ] || continue
 
