060594f826b0d2a179a3a157290663ea01b9dc22
19年梦醒 <3949379+getong@users.noreply.github.com>
2022-08-02 12:29:20 +0800

    add 25 slim

--- generate-stackbrew-library.sh
+++ generate-stackbrew-library.sh
@@ -72,7 +72,7 @@ for version in "${versions[@]}"; do
 	done
 	versionAliases+=( $version ${aliases[$version]:-} )
 
-	for variant in '' slim alpine otp-{21,22,25}{,-alpine}; do
+	for variant in '' slim alpine otp-{21,22,25}{,-alpine,-slim}; do
 		dir="$version${variant:+/$variant}"
 		[ -f "$dir/Dockerfile" ] || continue
 
