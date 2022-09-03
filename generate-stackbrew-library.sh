8d4a9b47774bca7eced3f7be361a2c1a78cf22c4
19年梦醒 <3949379+getong@users.noreply.github.com>
2022-09-03 09:53:09 +0800

    delete unsupported architecture

--- generate-stackbrew-library.sh
+++ generate-stackbrew-library.sh
@@ -87,7 +87,7 @@ for version in "${versions[@]}"; do
 		variantArches=( amd64 arm32v7 arm64v8 i386 s390x ppc64le )
 
 		case "$version" in
-			1.[876])
+			1.11|1.10|1.9|1.8|1.7|1.6)
                 variantArches=( ${variantArches[@]/ppc64le} )
                 variantArches=( ${variantArches[@]/s390x} )
 		esac
